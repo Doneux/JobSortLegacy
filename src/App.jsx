9d0a53df67a60b67078e541ba9e3599a708de929
Doneux <sjdoneux@gmail.com>
2025-08-27 17:27:22 -0700

    fix root repeated in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,3 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-
 export default function App() {
   return <h1>Hello from React!</h1>;
 }
-
-const root = createRoot(document.getElementById('root'));
-root.render(<App />);
